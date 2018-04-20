ef1fe02d518d71a78e70d0bfbdb4bae6b406d3c2
robuntu <robuntu@robuntu.ca>
2018-04-20 09:35:00 -0400

    first commit

---
+++ app/controllers/BooksController.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class BooksController extends Controller {
+
+
+    // for all books
+    public Result index() {
+        return TODO;
+    }
+
+    // to create books
+    public Result create() {
+        return TODO;
+    }
+
+    // to save backs
+    public Result save() {
+        return TODO;
+    }
+
+    public Result edit(Integer id) {
+        return TODO;
+    }
+
+    public Result update() {
+        return TODO;
+    }
+
+    public Result destroy(Integer id) {
+        return TODO;
+    }
+
+    // for book details
+    public Result show(Integer id) {
+        return TODO;
+    }
+
+
+}
